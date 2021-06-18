62a10e4f3475597abaab49ed30f0b7bfbc7cbe7a
Renan Gurgel <gurgel.renan@gmail.com>
2021-06-17 23:12:45 -0300

    Finish 'lifting-state-up' Section

--- app/javascript/packs/EventForm.jsx
+++ app/javascript/packs/EventForm.jsx
@@ -1,55 +1,16 @@
-import axios from 'axios';
 import React from 'react'
+import Event from './Event'
 
-class EventForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-      start_datetime: '',
-      location: ''
-    }
-  }
-
-  handleInput = (event) => {
-    const name = event.target.name;
-    const newState = {};
-    newState[name] = event.target.value;
-    this.setState(newState);
-    event.preventDefault();
-  }
-
-  handleSubmit = e => {
-    axios({
-      method: 'POST',
-      url: '/events',
-      data: { event: this.state },
-      headers: {
-        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
-      }
-    })
-    .then(response => {
-      this.props.handleNewEvent(response.data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-    e.preventDefault()
-  }
-
-  render() {
-    return (
-      <div>
-        <h4>Create an Event:</h4>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleInput} />
-          <input type="text" name="start_datetime" placeholder="Date" value={this.state.start_datetime} onChange={this.handleInput} />
-          <input type="text" name="location" placeholder="Location" value={this.state.location} onChange={this.handleInput} />
-          <button type="submit">Create Event</button>
-        </form>
-      </div>
-    )
-  }
-}
+const EventForm = props => (
+  <div>
+    <h4>Create an Event:</h4>
+    <form onSubmit={props.handleSubmit}>
+      <input type="text" name="title" placeholder="Title" value={props.title} onChange={props.handleInput} />
+      <input type="text" name="start_datetime" placeholder="Date" value={props.start_datetime} onChange={props.handleInput} />
+      <input type="text" name="location" placeholder="Location" value={props.location} onChange={props.handleInput} />
+      <button type="submit">Create Event</button>
+    </form>
+  </div>
+)
 
 export default EventForm
--- app/javascript/packs/Eventlite.jsx
+++ app/javascript/packs/Eventlite.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
 import EventsList from './EventliteList'
 import EventForm from './EventForm'
 
@@ -7,12 +9,42 @@ class Eventlite extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      events: this.props.events
+      events: this.props.events,
+      title: '',
+      start_datetime: '',
+      location: ''
     }
   }
 
+  handleInput = e => {
+    e.preventDefault()
+    const name = e.target.name
+    const newState = {}
+    newState[name] = e.target.value
+    this.setState(newState)
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    let newEvent = { title: this.state.title, start_datetime: this.state.start_datetime, location: this.state.location }
+    axios({
+      method: 'POST',
+      url: '/events',
+      data: { event: newEvent },
+      headers: {
+        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
+      }
+    })
+    .then(response => {
+      this.addNewEvent(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
   addNewEvent = (event) => {
-    const events = [event, ...this.state.events].sort(function(a, b) {
+    const events = [...this.state.events, event].sort(function(a, b){
       return new Date(a.start_datetime) - new Date(b.start_datetime)
     })
     this.setState({events: events})
@@ -21,9 +53,13 @@ class Eventlite extends React.Component {
   render() {
     return (
       <div>
-        <EventForm handleNewEvent={this.addNewEvent} />
+        <EventForm handleSubmit = {this.handleSubmit}
+          handleInput = {this.handleInput}
+          title = {this.state.title}
+          start_datetime = {this.state.start_datetime}
+          location = {this.state.location} />
         <EventsList events={this.state.events} />
-    </div>
+      </div>
     )
   }
 }
